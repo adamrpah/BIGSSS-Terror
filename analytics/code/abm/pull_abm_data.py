be90bfe0e7395aba2f30b03d87f750852aed374b
Adam Pah <adamrpah@gmail.com>
2018-08-02 10:05:32 +0200

    changing how omega is printed

--- analytics/code/abm/pull_abm_data.py
+++ analytics/code/abm/pull_abm_data.py
@@ -6,7 +6,7 @@ from scipy.stats import ks_2samp, binom_test
 max_file_count = 1000
 alphas = np.arange(0.5, 1.4, 0.1)
 betas = np.arange(5, 10.5, 0.5)
-omegas = np.arange(0, 1.1, 0.1)
+omegas = [0, 0.1, 1]
 #Testing
 #alphas = [0.5]
 #betas = [5.5]
@@ -27,7 +27,7 @@ for country in ['Afghanistan', 'Colombia', 'Iraq']:
             for omega in omegas:
                 group_pvalues = {g:[] for g in empirical_data.keys() if len(empirical_data[g])>1}
                 #Read through all the parameter files
-                for parameter_file in glob.glob('../../results/abm_runs/%s_%.1f_%.1f_%.1f_*.csv' % (country, alpha, beta, omega)):
+                for parameter_file in glob.glob('../../results/abm_runs/%s_%.1f_%.1f_%s_*.csv' % (country, alpha, beta, str(omega))):
                     #Run dataframe is 
                     run_df = pd.read_csv(parameter_file, header=None, names=['group', 'tick', 'attack'])
                     for group, gdf in run_df.groupby('group'):
@@ -39,18 +39,20 @@ for country in ['Afghanistan', 'Colombia', 'Iraq']:
                             sim_interevent = np.diff(timeset)
                             D, p = ks_2samp(sim_interevent, empirical_data[group])
                             group_pvalues[group].append(p)
-                #Do the pass fail
-                pass_groups, fail_groups = 0, 0
-                for group, pval_list in group_pvalues.items():
-                    fail_percentage = len([p for p in pval_list if p <0.05]) / max_file_count
-                    if fail_percentage >0.05:
-                        fail_groups += 1
-                    else:
-                        pass_groups += 1
-                #Run the binomial test
-                p = binom_test(pass_groups, pass_groups + fail_groups)
-                #keep it
-                record.append([country, alpha, beta, omega, p])
+                #Protect against a deadfile
+                if len([v for v in group_pvalues.items() if v==[]]) != len(group_pvalues.keys()):
+                    #Do the pass fail
+                    pass_groups, fail_groups = 0, 0
+                    for group, pval_list in group_pvalues.items():
+                        fail_percentage = len([p for p in pval_list if p <0.05]) / max_file_count
+                        if fail_percentage >0.05:
+                            fail_groups += 1
+                        else:
+                            pass_groups += 1
+                    #Run the binomial test
+                    p = binom_test(pass_groups, pass_groups + fail_groups)
+                    #keep it
+                    record.append([country, alpha, beta, omega, p])
 #create the dataframe and write it
 with open('../../results/abm_pvalue_analysis.csv', 'w') as wfile:
     print('country,alpha,beta,omega,pvalue', file = wfile)
