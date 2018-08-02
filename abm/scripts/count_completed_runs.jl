be9931c7f9314551562f76d38334d7df7fb43cda
Nicolas Payette <nicolaspayette@gmail.com>
2018-08-02 10:25:51 +0200

    fix run count script

--- abm/scripts/count_completed_runs.jl
+++ abm/scripts/count_completed_runs.jl
@@ -7,7 +7,7 @@ input_df = CSV.read(input_file, header = 7, rows_for_type_detect = 1000)
 input_df[:country] = input_df[Symbol("input-folder")] # rename column
 
 output_df = by(input_df, [:country, :alpha, :beta, :omega]) do df
-  DataFrame(completed = sum(df[Symbol("[step]")] .== 1826), total = length(df))
+  DataFrame(completed = sum(df[Symbol("[step]")] .== 1826), total = nrow(df))
 end
 
 CSV.write(output_file, output_df)
