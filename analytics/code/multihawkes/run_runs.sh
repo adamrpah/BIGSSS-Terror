0b7a711098f7012eebf1b3caeae9d0aefd2d2c36
root <adam pah>
2018-10-12 16:16:05 +0000

    typo in run script

--- analytics/code/multihawkes/run_runs.sh
+++ analytics/code/multihawkes/run_runs.sh
@@ -3,5 +3,5 @@ nohup python multihawkes.py ../../data/Iraq_abm_events.csv ../../results/multiha
 sleep 2
 nohup python multihawkes.py ../../data/Afghanistan_abm_events.csv ../../results/multihawkes/tol_runs  --threshold 0.0001 > afg.out &
 sleep 2
-nohup python multihawkes.py ../../data/Colombia_abm_events.csv ../../results/multihawkes/tol_runs  --threhsold 0.0001 > col.out &
+nohup python multihawkes.py ../../data/Colombia_abm_events.csv ../../results/multihawkes/tol_runs  --threshold 0.0001 > col.out &
 sleep 2
