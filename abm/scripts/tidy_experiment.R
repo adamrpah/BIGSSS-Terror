8664931b0369d90589b7b9405394c76db6196772
Nicolas Payette <nicolaspayette@gmail.com>
2018-10-20 11:45:02 +0200

    process experiment results in chunks

--- abm/scripts/tidy_experiment.R
+++ abm/scripts/tidy_experiment.R
@@ -1,25 +1,34 @@
 library(tools)
-library(glue)
 library(tidyverse)
 
-file <- file.path(
-  path.expand("~/workspace/BIGSSS-Terror/abm/outputs/20181002"),
-  "results_20181002.csv"
+input_file <- file.path(
+  path.expand("~/workspace/BIGSSS-Terror/abm/outputs/20181009"),
+  "results_20181009.csv"
 )
 
-df <- read_csv(file, skip = 6) %>%
-  mutate(attacks = map(`attacks-csv`, read_csv)) %>%
-  unnest %>%
-  select(
-    run = `[run number]`,
-    final_step = `[step]`,
-    country = `input-folder`,
-    alpha, beta, omega, group, step, num_attacks
-  ) %>%
-  write_csv(file_path_sans_ext(file) %>% glue("_tidy.csv"))
+output_file <- str_glue(file_path_sans_ext(input_file), "_tidy.csv")
 
-df %>% # Just check how many reps we have
-  select(run, alpha, beta, omega, country) %>%
-  distinct() %>%
-  count(alpha, beta, omega, country) %>%
-  View()
+split_col <- "omega"
+
+df_list <-
+  read_csv(input_file, skip = 6) %>%
+  split(.[[split_col]])
+
+walk(df_list, function(df) {
+  gc(verbose = TRUE) # this helps a lot...
+  print(df) # just to see where we're at
+  df %>%
+    mutate(attacks = map(`attacks-csv`, read_csv)) %>%
+    unnest() %>%
+    select(
+      run = `[run number]`,
+      final_step = `[step]`,
+      country = `input-folder`,
+      alpha, beta, omega, group, step, num_attacks
+    ) %>%
+    write_csv(
+      output_file,
+      # write in 'append' mode except for the first chunk:
+      append = (.[[split_col]][1] != df_list[[1]][[split_col]][1])
+    )
+})
