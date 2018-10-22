a48d0f6028fec34c6eb79e66e2742b55988e2083
Adam Pah <adamrpah@gmail.com>
2018-10-22 16:37:20 -0500

    final data for multihawkes networks

--- analytics/code/multihawkes/srf_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nohup python multihawkes_srf.py ../../data/Iraq_abm_events.csv hawkes_srf/  >iraq.out &
-nohup python multihawkes_srf.py ../../data/Afghanistan_abm_events.csv hawkes_srf/  > afg.out &
-nohup python multihawkes_srf.py ../../data/Colombia_abm_events.csv hawkes_srf/  > col.out &
